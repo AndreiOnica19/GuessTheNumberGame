d2e55c6b20edf2d18c8132daa3e09307fc148526
AndreiOnica <onica.andrei19@yahoo.com>
2024-04-06 15:05:14 +0300

    Added attempts counter

--- src/Game.java
+++ src/Game.java
@@ -6,16 +6,18 @@ public class Game {
         Random rnd = new Random();
         int hiddenNumber = rnd.nextInt(101);
         Scanner sc = new Scanner(System.in);
+        int counter = 0;
 
         while (true) {
             System.out.println("Guess a number between 1 and 100!");
             int guessedNumber = sc.nextInt();
+            counter++;
             if (guessedNumber > hiddenNumber) {
                 System.out.println("Less");
             } else if (guessedNumber < hiddenNumber) {
                 System.out.println("More");
             } else {
-                System.out.println("You guessed!");
+                System.out.println("You guessed! You used " + counter + " attempts.");
                 break;
             }
         }
