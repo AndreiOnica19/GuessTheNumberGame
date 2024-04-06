c03164f91f6b5287027ad9280a03a7a402b4b17f
AndreiOnica <onica.andrei19@yahoo.com>
2024-04-06 14:45:26 +0300

    Added conditions

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) {
         int hiddenNumber = 88;
-
+        System.out.println("Guess a number between 1 and 100!");
+        Scanner sc = new Scanner(System.in);
+        int guessedNumber = sc.nextInt();
+        if (guessedNumber > hiddenNumber) {
+            System.out.println("Less");
+        } else if (guessedNumber < hiddenNumber) {
+            System.out.println("More");
+        } else {
+            System.out.println("You guessed!");
+        }
     }
 }
