f079941f80eed90c15eb1fc24f2e805d8d3969dd
AndreiOnica <onica.andrei19@yahoo.com>
2024-04-06 14:48:34 +0300

    Added while loop

--- src/Game.java
+++ src/Game.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 public class Game {
     public static void main(String[] args) {
         int hiddenNumber = 88;
-        System.out.println("Guess a number between 1 and 100!");
         Scanner sc = new Scanner(System.in);
-        int guessedNumber = sc.nextInt();
-        if (guessedNumber > hiddenNumber) {
-            System.out.println("Less");
-        } else if (guessedNumber < hiddenNumber) {
-            System.out.println("More");
-        } else {
-            System.out.println("You guessed!");
+
+        while (true) {
+            System.out.println("Guess a number between 1 and 100!");
+            int guessedNumber = sc.nextInt();
+            if (guessedNumber > hiddenNumber) {
+                System.out.println("Less");
+            } else if (guessedNumber < hiddenNumber) {
+                System.out.println("More");
+            } else {
+                System.out.println("You guessed!");
+                break;
+            }
         }
     }
 }
