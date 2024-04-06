0f4d32e69b2e8d8e6915ab199b9cc89a8df1f44b
AndreiOnica <onica.andrei19@yahoo.com>
2024-04-06 15:09:35 +0300

    Added lives counter

--- src/Game.java
+++ src/Game.java
@@ -7,15 +7,22 @@ public class Game {
         int hiddenNumber = rnd.nextInt(101);
         Scanner sc = new Scanner(System.in);
         int counter = 0;
+        int lives = 8;
 
         while (true) {
             System.out.println("Guess a number between 1 and 100!");
             int guessedNumber = sc.nextInt();
             counter++;
+            if (lives == 0){
+                System.out.println("Game over!");
+                break;
+            }
             if (guessedNumber > hiddenNumber) {
                 System.out.println("Less");
+                lives--;
             } else if (guessedNumber < hiddenNumber) {
                 System.out.println("More");
+                lives--;
             } else {
                 System.out.println("You guessed! You used " + counter + " attempts.");
                 break;
